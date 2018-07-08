dbc3d6e79148013177b16b105808d478cfbdcc71
frian <a@frian.org>
2018-07-09 00:08:45 +0200

    added dynamic hamburger menu button - fixes and cleaning

--- web/js/script.js
+++ web/js/script.js
@@ -1,9 +1,15 @@
 $(function() {
 
-    // var myFullpage = new fullpage('#fullpage', {
-    //     licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-    //     easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)'
-    //     // navigation: true,
-    //     // navigationPosition: 'right',
-    // });
+    var myFullpage = new fullpage('#fullpage', {
+        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+        easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)'
+        // navigation: true,
+        // navigationPosition: 'right',
+    });
+
+    var $hamburger = $(".hamburger");
+    $hamburger.on("click", function(e) {
+        $hamburger.toggleClass("is-active");
+        // Do something else, like open/close menu
+    });
 });
