cbde4a7a8ce484b7723fa3b276a93edc05e671e2
frian <a@frian.org>
2018-07-08 19:15:45 +0200

    added files

---
+++ README.md
@@ -0,0 +1,4 @@
+.
+=
+
+A Symfony project created on May 8, 2018, 12:12 pm.
---
+++ web/js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+
+    var myFullpage = new fullpage('#fullpage', {
+        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+        easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)'
+        // navigation: true,
+        // navigationPosition: 'right',
+    });
+});
