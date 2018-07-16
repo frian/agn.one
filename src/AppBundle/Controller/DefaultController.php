5951c8bf8308389d21e2392da50631a406229f3d
frian <a@frian.org>
2018-07-16 21:44:58 +0200

    added landing page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,7 +9,18 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="landing")
+     */
+    public function landingAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/landing.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/home", name="homepage")
      */
     public function indexAction(Request $request)
     {
